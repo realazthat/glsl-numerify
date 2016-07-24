7fe5e180237a3151833e0e8f0ffe1bde79b43146
realazthat <realazthat@gmail.com>
2016-07-24 18:29:13 -0500

    Initial commit

---
+++ glsl-numerify-demo.js
@@ -0,0 +1,129 @@
+
+const resl = require('resl');
+const regl = require('regl')({
+  extensions: ['OES_texture_float', 'OES_texture_float_linear']
+});
+
+const numerify = require('./glsl-numerify.js');
+
+
+
+const quadVerts = [
+  [-1.0, -1.0, 0.0], [-1.0, 1.0, 0.0], [1.0, -1.0, 0.0], [1.0, 1.0, 0.0]
+];
+
+const quadUVs = [
+  [0.0, 1.0], [0.0, 0.0], [1.0, 1.0], [1.0, 0.0]
+];
+
+const quadIndices = [
+  [3, 1, 0], [0, 2, 3]
+];
+
+const quadVertexShader = `
+  precision mediump float;
+  attribute vec3 position;
+  attribute vec2 uv;
+  varying vec2 vUv;
+  
+  void main() {
+    vUv = uv;
+    gl_Position = vec4(position, 1);
+  }
+`;
+
+const quadFragShader = `
+  precision mediump float;
+  varying vec2 vUv;
+  uniform sampler2D tex;
+  void main () {
+    gl_FragColor = texture2D(tex,vUv);
+  }
+`;
+
+function makeFBORgbUint8 ({width, height}) {
+  return regl.framebuffer({
+    color: regl.texture({
+      width: width,
+      height: height,
+      stencil: false,
+      colorFormat: 'rgba',
+      colorType: 'uint8',
+      depth: false,
+      wrap: 'clamp'
+    })
+  });
+}
+
+resl({
+  manifest: {
+    texture: {
+      type: 'image',
+      src: 'assets/numerify-32x32-exemplar.png',
+      parser: (data) => regl.texture({
+        data: data,
+        mag: 'nearest',
+        min: 'nearest'
+      })
+    }, digits_texture: {
+      type: 'image',
+      src: 'assets/digits.bmp',
+      parser: (data) => regl.texture({
+        data: data,
+        mag: 'nearest',
+        min: 'nearest'
+      })
+    }
+  },
+  onDone: ({texture, digits_texture}) => {
+    console.log(texture.width);
+    console.log(digits_texture.width);
+
+
+
+
+
+    let fbo = makeFBORgbUint8({width: texture.width * 16, height: texture.height * 16});
+
+    let frag = numerify.makeFrag({ multiplier: 256,
+                                    sourceSize: `vec2(${texture.width}, ${texture.height})`,
+                                    destinationSize: `vec2(${texture.width * 16}, ${texture.height * 16})`,
+                                    destinationCellSize: 'vec2(16, 16)'});
+    let vert = numerify.makeVert();
+
+    console.log(frag);
+
+    const drawToDestination = regl({
+      frag: frag,
+      vert: vert,
+      attributes: {
+        position: quadVerts,
+        uv: quadUVs
+      },
+      elements: quadIndices,
+      uniforms: {
+        source_texture: regl.prop('source_texture'),
+        digits_texture: regl.prop('digits_texture')
+      },
+      framebuffer: regl.prop('fbo')
+    });
+
+    drawToDestination({ source_texture: texture,
+                        digits_texture: digits_texture,
+                        fbo: fbo});
+
+    const drawToScreen = regl({
+      frag: quadFragShader,
+      vert: quadVertexShader,
+      attributes: {
+        position: quadVerts,
+        uv: quadUVs
+      },
+      elements: quadIndices,
+      uniforms: {
+        tex: regl.prop('texture')
+      }
+    });
+    drawToScreen({texture: fbo.color[0]});
+  }
+});
---
+++ glsl-numerify.js
@@ -0,0 +1,82 @@
+
+function makeVert () {
+  let vert = `
+    precision mediump float;
+    attribute vec3 position;
+    attribute vec2 uv;
+    varying vec2 vUv;
+    
+    void main() {
+      vUv = uv;
+      gl_Position = vec4(position, 1);
+    }
+  `;
+  return vert;
+}
+
+function makeFrag ({multiplier, sourceSize, destinationCellSize, destinationSize}) {
+  let frag = `
+    precision highp float;
+    varying vec2 vUv;
+    uniform sampler2D digits_texture;
+    uniform sampler2D source_texture;
+
+    const vec2 digit_size = vec2(4,7);
+    const vec2 digit_texture_size = vec2(64, 64);
+    const vec2 source_size = ${sourceSize};
+    const vec2 destination_cell_size = ${destinationCellSize};
+    const vec2 destination_size = ${destinationSize};
+    const vec2 destination_view_size = ${sourceSize} * ${destinationCellSize};
+    const vec4 multiplier = vec4(${multiplier});
+    void main () {
+
+      highp vec2 screen_rel_pixel = floor(vUv*destination_size);
+      highp vec2 screen_rel_cell = floor(screen_rel_pixel / destination_cell_size);
+      highp vec2 screen_rel_cell_lower_pixel = screen_rel_cell * destination_cell_size;
+      highp vec2 screen_rel_cell_upper_pixel = screen_rel_cell_lower_pixel + destination_cell_size;
+      highp vec2 screen_rel_cell_center_pixel = screen_rel_cell_lower_pixel + (destination_cell_size / 2.0);
+      highp vec2 cell_rel_offset_pixel = screen_rel_pixel - screen_rel_cell_lower_pixel;
+
+      highp vec2 source_uv = (screen_rel_cell + .5)/source_size;
+
+      gl_FragColor = vec4(1,1,1,1);
+
+
+      if (any(greaterThan(screen_rel_pixel, destination_view_size)))
+        return;
+
+      if (any(lessThan(cell_rel_offset_pixel, vec2(1))) || any(greaterThan(cell_rel_offset_pixel, destination_cell_size)))
+      {
+        gl_FragColor = vec4(0,0,1,1);
+        return;
+      }
+
+      vec4 source_pixel = texture2D(source_texture, source_uv);
+      vec4 denormed_source_value = source_pixel*multiplier;
+
+      float cell_rel_right_offset_pixel = destination_cell_size.x - 1.0 - cell_rel_offset_pixel.x;
+      int digit_index = int(floor(cell_rel_right_offset_pixel / digit_size.x));
+      int digit_rel_x = int(digit_size.x) - int(floor(mod(cell_rel_right_offset_pixel, digit_size.x)));
+      /// center of cell, plus half digit hight.
+      int cell_rel_digit_bottom_y = int((destination_cell_size.y / 2.0) + digit_size.y / 2.0);
+      int cell_rel_dist_from_digit_bottom = cell_rel_digit_bottom_y - int(cell_rel_offset_pixel.y);
+
+      int sigdigits = int(floor(denormed_source_value.r / pow(10.0, float(digit_index))));
+      if (sigdigits == 0)
+          return;
+      int digit = int(mod(float(sigdigits), 10.0));
+
+      vec2 digits_uv = vec2(float(digit)*digit_size.x, digit_size.y - float(cell_rel_dist_from_digit_bottom)) / digit_texture_size;
+
+
+      float cell_top_border = floor((destination_cell_size.y - digit_size.y) / 2.0);
+      digits_uv = (vec2((digit*int(digit_size.x)) + digit_rel_x, destination_cell_size.y - cell_top_border - cell_rel_offset_pixel.y) + vec2(.5))/digit_texture_size;
+
+      gl_FragColor.rg = digits_uv;
+      gl_FragColor = texture2D(digits_texture, digits_uv);
+    }
+  `;
+  return frag;
+}
+
+module.exports = { makeFrag: makeFrag, makeVert: makeVert };
